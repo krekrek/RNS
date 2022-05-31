0dd4b9e492ca48a89bf3042e9f0da01ca6c0bbd8
krekrek <krekrek@naver.com>
2022-06-01 06:10:07 +0900

    ex.more_about_printing

--- ex.more_about_printing.rs
+++ ex.more_about_printing.rs
@@ -1,4 +1,18 @@
 fn main() {
+    let my_variable = 27;
+    println!("{:x} {:o} {:b}", my_variable,my_variable,my_variable);
+    // 위와 같이 {:-}는 C언어에서의 포멧스트링(%) 처럼 사용되며 아래와 같은 옵션들이 있음.
+    // - ``, which uses the `Display` trait
+    // - `?`, which uses the `Debug` trait
+    // - `e`, which uses the `LowerExp` trait
+    // - `E`, which uses the `UpperExp` trait
+    // - `o`, which uses the `Octal` trait
+    // - `p`, which uses the `Pointer` trait
+    // - `b`, which uses the `Binary` trait
+    // - `x`, which uses the `LowerHex` trait
+    // - `X`, which uses the `UpperHex` trait
+
+
     let title = "TODAY'S NEWS";
     println!("{:-^30}", title); // 30개의 '-' 사이에 해당 문자열을 출력
     let bar = "|";
