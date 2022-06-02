b56d6ddf3e7e53ce6b065fd29e3b021242a60c75
krekrek <krekrek@naver.com>
2022-06-02 23:55:27 +0900

    ex.more_about_printing

--- ex.more_about_printing.rs
+++ ex.more_about_printing.rs
@@ -13,11 +13,11 @@ fn main() {
     // - `X`, which uses the `UpperHex` trait
 
 
-    let title = "TODAY'S NEWS";
+    let title = "TODAY'S LANGUAGE";
     println!("{:-^30}", title); // 30개의 '-' 사이에 해당 문자열을 출력
     let bar = "|";
     println!("{: <15}{: >15}", bar, bar); // 15개의 공백 왼쪽과 15개의 공백 오른쪽에 해당 문자를 출력
-    let a = "SEOUL";
-    let b = "TOKYO";
+    let a = "PYTHON";
+    let b = "RUST";
     println!("{:-<15}{:->15}", a, b); // 15개의 '-'왼쪽에 a문자열을, 오른쪽에 b문자열을 출력
 } 
